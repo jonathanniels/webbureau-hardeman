62da4e31265749e6660f3de865a7e5e1f2fedc76
nielshardeman <jonathanniels@gmail.com>
2020-05-27 11:12:32 +0200

    Removed all demo content, templates and components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import LayoutTrydo from '../components/LayoutTrydo'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <LayoutTrydo>
+    <div class="about-area rn-section-gap bg_color--5" id="about">
+        <div class="about-wrapper">
+            <div class="container">
+                <div class="row row--35 align-items-center">
+                    <div class="col-12">
+                        <div class="about-inner inner">
+                            <div class="section-title">
+                                <h2 class="title">Page not found</h2>
+                                <p class="description">The page that you are looking for can&#39;t be found</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </Layout>
+  </LayoutTrydo>
 )
 
 export default NotFoundPage
